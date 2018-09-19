539e21f662384fd037673b3138b793b2fec1cf47
hudson-m <hudson.veiga@gmail.com>
2018-09-19 12:30:22 -0300

    Adicionado tela de login, porem esta com erro com o css

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
+var session = require('express-session');
+var path = require('path');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(__dirname + '/public'));
 
 // Banco de dados
@@ -30,6 +32,7 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+//---------------LOGIN---------------
 //Tranforma senha em hash para quando for para o database
 UserSchema.pre('save', function (next) {
     var user = this;
@@ -53,17 +56,43 @@ app.use(session({
     saveUninitialized: false
   }));
 
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+    User.findOne({ email: email })
+      .exec(function (err, user) {
+        if (err) {
+          return callback(err)
+        } else if (!user) {
+          var err = new Error('User not found.');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        })
+      });
+  }
+
+//---------------FIM DO LOGIN---------------
+
 // template engine
 app.set('view engine', 'ejs');
-
+app.set('view engine', 'html');
 //middleware
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 //routes
 app.get('/chat', (req, res) => {
     res.render('index');
 });
-
+app.get('/', (req,res) =>{
+    res.sendFile("views/login.html", {"root": __dirname});
+});
 app.post('/', (req, res) =>{
     if (req.body.email &&
         req.body.username &&
